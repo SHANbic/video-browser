324f2ed017306a3a4abcecd66bd4bb2f1746d181
Pierre Lambert <supagoof924@hotmail.com>
2019-05-07 23:10:20 +0200

    [ FEATURE ] Created basic App and SearchBar components

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import SearchBar from './SearchBar'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>hello</h1>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
