f82f5c880026aa6ad486ace24266b422f710048d
Pierre Lambert <supagoof924@hotmail.com>
2019-05-08 20:52:41 +0200

    [ FEATURE ] Updated VideoDetail component to display actual videos

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -3,14 +3,19 @@ import React from "react";
 const VideoDetail = ({ video }) => {
   if (!video) {
     return <div>Loading...</div>;
-  } else {
-    return (
-      <div className="ui segment">
-        <h4 className="ui header">{video.snippet.title}</h4>
-        <p>{video.snippet.description}</p>
-      </div>
-    );
   }
+
+  const VideoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
+  return (
+    <div className="ui segment">
+      <div className="ui embed">
+        <iframe src={VideoSrc} />
+      </div>
+      <h4 className="ui header">{video.snippet.title}</h4>
+      <p>{video.snippet.description}</p>
+    </div>
+  );
 };
 
 export default VideoDetail;
